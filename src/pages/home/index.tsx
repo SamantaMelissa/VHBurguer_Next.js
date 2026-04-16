6766fce29b0d81afd4d50811de4241db05b2b467
Samanta Melissa <samanta.melissa13@gmail.com>
2026-04-16 09:13:49 -0300

    Subindo css do header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/header/header";
+
 //ESTRUTURA PADRÃO!
 const Home = () => {
     return (
         <>
-           <p>aaaaaaaaaaaa</p>
+           <Header/>
         </>
     )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,11 +3,11 @@ import styles from "./login.module.css";
 const Login = () => {
     return (
         <>
-            <main className={styles.main}>
+            <main id={styles.main}>
                 <img src="../imgs/hamburguer_login.png" alt="Hambúrguer com ingredientes flutuando em camadas sobre fundo escuro."/>
-                <div className={styles.campo_login}>
+                <div id={styles.campo_login}>
                     <h1>Login</h1>
-                    <form className={styles.formulario}>
+                    <form id={styles.formulario}>
                         <div className={styles.campo_form}>
                             <label htmlFor="email">E-mail</label>
                             <input type="text" name="email" placeholder="email@exemplo.com" required />
@@ -16,7 +16,7 @@ const Login = () => {
                             <label htmlFor="senha">Senha</label>
                             <input type="password" name="senha" placeholder="*******" required />
                         </div>
-                        <a className={styles.esq_senha} href="">Esqueceu sua senha?</a>
+                        <a id={styles.esq_senha} href="">Esqueceu sua senha?</a>
                         <button>Entrar</button>
                     </form>
                 </div>
