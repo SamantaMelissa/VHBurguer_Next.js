54f6f41635f42233f64bd0b7867ebf5d53ac19d2
Samanta Melissa <samanta.melissa13@gmail.com>
2026-04-22 08:52:04 -0300

    subindo base par ao componente lista-produto

--- src/components/card-produto/card-produto.tsx
+++ src/components/card-produto/card-produto.tsx
@@ -2,7 +2,17 @@ import styles from "./card-produto.module.css"
 
 const CardProduto = () => {
     return(
-        <></>
+        <article>
+            <img src="" alt="" />
+            <h3></h3>
+            <p></p>
+            <div>
+                <p></p>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </article>
     )
 }
 
