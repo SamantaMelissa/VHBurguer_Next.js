421a282d0d5e0264c68146477b56a948d15efe0e
Samanta Melissa <samanta.melissa13@gmail.com>
2026-04-23 10:04:31 -0300

    Subindo tela de detalhe de produto

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./header.module.css";
 
 const Header = () => {
@@ -6,10 +7,10 @@ const Header = () => {
             <div className={`${styles.container} layout_guide`}>
                 <img src="../imgs/Logo_VH_Burguer.svg" alt="Logo do VH Burguer que contém como plano de fundo um hamburguer" id={styles.logo}/>
                 <nav id={styles.nav_menu}>
-                    <a href="">Destaques</a>
-                    <a href="">Cardápio</a>
-                    <a href="">Unidades</a>
-                    <a href="">Login</a>
+                    <a href="#destaques">Destaques</a>
+                    <a href="#cardapio">Cardápio</a>
+                    <a href="#unidades">Unidades</a>
+                    <Link href="/login">Login</Link>
                 </nav>
                 <button id={styles.btn_icon}>
                     <img src="../imgs/icon_hamburguer.svg" alt="Ícone que representa um hamburguer para acessar o menu lateral" />
--- src/components/lista-produto/lista-produto.tsx
+++ src/components/lista-produto/lista-produto.tsx
@@ -2,6 +2,7 @@ import CardProduto from "../card-produto/card-produto";
 import styles from "./lista-produto.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSliders } from '@fortawesome/free-solid-svg-icons'
+import Link from "next/link";
 
 const ListaProduto = () => {
     return (
@@ -12,8 +13,8 @@ const ListaProduto = () => {
                     <FontAwesomeIcon icon={faSliders} />
                 </button>
                 <div id={styles.botoes_direita}>
-                    <button className={styles.botao}>Promoções</button>
-                    <button className={styles.botao}>Produtos</button>
+                    <Link className={styles.botao} href="/promocoes">Promoções</Link>
+                    <Link className={styles.botao} href="/produto">Adicionar produtos</Link>
                 </div>
             </div>
             <div id={styles.cards_produtos}>
